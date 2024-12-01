4a1055401e707c9425b275a7a118757632d2c8b2
Tilo Dietrich <git@tilod.dev>
2024-12-01 22:00:52 +0100

    Add alternative solutions

--- Python/day1_puzzle2.py
+++ Python/day1_puzzle2.py
@@ -1,9 +1,12 @@
+import functools
+
 with open("../input/day1.txt", "r") as f:
     list1, list2 = zip(*[line.strip().split("   ") for line in f.readlines()])
 
 list1 = list(map(int, list1))
 list2 = list(map(int, list2))
 
-result = sum(i * list2.count(i) for i in list1)
+result = functools.reduce(lambda s, i: s + i * list2.count(i), list1, 0)
+# result = sum(i * list2.count(i) for i in list1)
 
 print(result)
--- Ruby/day1_puzzle2.rb
+++ Ruby/day1_puzzle2.rb
@@ -8,5 +8,6 @@ list1.map!(&:to_i)
 list2.map!(&:to_i)
 
 result = list1.inject(0) { |s, i| s + i * list2.count(i) }
+# result = list1.map { |i| i * list2.count(i) }.sum
 
 puts result
